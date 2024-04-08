a3628a4a61d3fa9b6db9745030674d427d61b908
Law P <110138754+law-p@users.noreply.github.com>
2024-04-08 16:19:27 +0100

    Added site icon

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary" data-bs-theme="dark">
     <div class="container">
-        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span>{{config('app.name')}}</a>
+        <a class="navbar-brand fw-light" href="/"><span class="fas fa-pen me-1"> </span>{{config('app.name')}}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
