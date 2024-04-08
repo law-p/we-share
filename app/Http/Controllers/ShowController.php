8f9f983a2f7823f2d38ccba69573c43e65ec01ec
Law P <110138754+law-p@users.noreply.github.com>
2024-04-08 17:43:24 +0100

    debug issues

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LoginController extends Controller
+class ShowController extends Controller
 {
     public function index(){
         return view('show');
