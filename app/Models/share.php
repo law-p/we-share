cc34ac1ab87ebee338069007f051782caf63e33b
Law P <110138754+law-p@users.noreply.github.com>
2024-04-08 17:53:58 +0100

    psr4 error resolved

--- app/Models/share.php
+++ app/Models/share.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-class share extends Model
+class Share extends Model
 {
     use HasFactory;
      protected $fillable = ['content', 'user_id'];
